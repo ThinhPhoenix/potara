dddd7c1bcdc7a62f0429b32ca50c88c7d5a5cd96
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-22 21:30:09 +0700

    add installation script for Potara CLI

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+REPO="ThinhPhoenix/potara-cli"
+VERSION="v1.0.0"
+URL="https://github.com/$REPO/releases/download/$VERSION/potara-cli.tar.gz"
+
+curl -L $URL -o potara-cli.tar.gz
+tar -xzf potara-cli.tar.gz
+chmod +x potara
+sudo mv potara /usr/local/bin/potara
+echo "Potara installed successfully!"
