da6d56b3fb5213767b4422ec9645b0a3966b3c40
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-23 15:28:55 +0700

    Update homepage URL in Potara formula

--- homebrew/potara.rb
+++ homebrew/potara.rb
@@ -1,7 +1,7 @@
 class Potara < Formula
     desc "A simple CLI for proxying multiple ports"
-    homepage "https://github.com/ThinhPhoenix/potara-cli/"
-    url "https://github.com/ThinhPhoenix/potara-cli/releases/download/v1.0.0/potara.tgz"
+    homepage "https://github.com/ThinhPhoenix/potara/"
+    url "https://github.com/ThinhPhoenix/potara/releases/download/v1.0.0/potara.tgz"
     sha256 "a897f06cc129c914d92c612488660e9ffde6c02140e3711d74c35984b1b87c36"
   
     def install
