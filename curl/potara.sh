661d0cdfbfb66f7b2bef0554280a05077006feab
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-23 15:39:15 +0700

    Update download URL extension in Potara installation script

--- curl/potara.sh
+++ curl/potara.sh
@@ -2,7 +2,7 @@
 set -e
 REPO="ThinhPhoenix/potara"
 VERSION="v1.0.0"
-URL="https://github.com/$REPO/releases/download/$VERSION/potara.tar.gz"
+URL="https://github.com/$REPO/releases/download/$VERSION/potara.tgz"
 
 curl -L $URL -o potara.tar.gz
 tar -xzf potara.tar.gz
